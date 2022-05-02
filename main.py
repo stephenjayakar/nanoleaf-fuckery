b90aca5513a9b620010e1491245b792dfaea5055
Stephen Jayakar <aj12ay@gmail.com>
2022-05-01 19:42:16 -0700

    yikes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import nanoleafapi
 import random
+import keyboard
 import time
 
 
@@ -10,17 +11,52 @@ digital_twin = nanoleafapi.NanoleafDigitalTwin(nl)
 panels = nl.get_ids()
 times = []
 
+
 while True:
-    start = time.time()
-    for panel_id in panels:
-        # set each panel to a random RGB value
+    event = keyboard.read_event()
+    if event.event_type == keyboard.KEY_DOWN:
+        key = event.name
+        # print(f'Pressed: {event}')
+        # print(f'Pressed: {event.scan_code}')
+        start = time.time()
+        # panel_id = random.choice(panels)
+        panel_id = panels[event.scan_code % len(panels)]
+        panel_id2 = panels[(event.scan_code + 1) % len(panels)]
+        panel_id3 = panels[(event.scan_code + 2) % len(panels)]
         digital_twin.set_color(panel_id, (
-            random.randint(0,255),
-            random.randint(0,255),
-            random.randint(0,255),
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255),
+        ))
+        digital_twin.set_color(panel_id2, (
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255),
+        ))
+        digital_twin.set_color(panel_id3, (
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255),
         ))
 
-    digital_twin.sync()
-    elapsed_time = time.time() - start
-    print(f'elapsed time: {elapsed_time}')
-    times.append(elapsed_time)
+        digital_twin.sync()
+        elapsed_time = time.time() - start
+        print(f'elapsed time: {elapsed_time}')
+        times.append(elapsed_time)
+        if key == 'q':
+            break
+
+# while True:
+#     start = time.time()
+#     for panel_id in panels:
+#         # set each panel to a random RGB value
+#         digital_twin.set_color(panel_id, (
+#             random.randint(0,255),
+#             random.randint(0,255),
+#             random.randint(0,255),
+#         ))
+
+#     digital_twin.sync()
+#     elapsed_time = time.time() - start
+#     print(f'elapsed time: {elapsed_time}')
+#     times.append(elapsed_time)
