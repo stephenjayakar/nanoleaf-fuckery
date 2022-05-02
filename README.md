87ed2ef8395f41b68bb1da844478de420c9c0042
Stephen Jayakar <aj12ay@gmail.com>
2022-05-01 18:39:49 -0700

    quick benchmark

---
+++ README.md
@@ -0,0 +1,3 @@
+# Benchmark for setting random colors
+
+100ms on average to update all the panels.
--- main.py
+++ main.py
@@ -8,16 +8,19 @@ ip = '192.168.4.30'
 nl = nanoleafapi.Nanoleaf(ip)
 digital_twin = nanoleafapi.NanoleafDigitalTwin(nl)
 panels = nl.get_ids()
+times = []
 
 while True:
+    start = time.time()
     for panel_id in panels:
-        start = time.time()
         # set each panel to a random RGB value
         digital_twin.set_color(panel_id, (
             random.randint(0,255),
             random.randint(0,255),
             random.randint(0,255),
         ))
-        end = time.time() - start
 
     digital_twin.sync()
+    elapsed_time = time.time() - start
+    print(f'elapsed time: {elapsed_time}')
+    times.append(elapsed_time)
